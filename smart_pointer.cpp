e1c3893de46b17afa97ea35698004267fad70f8f
Rajat Goswami <rajatgoswami.dev@gmail.com>
2021-09-21 05:09:56 -0400

    Adding implementation of shared_ptr

--- smart_pointer.cpp
+++ smart_pointer.cpp
@@ -22,7 +22,79 @@ template<typename T> class uniq_ptr
             return raw_ptr;
         }
 
+        T* get(){
+            return raw_ptr;
+        }
+
     private:
+        T* raw_ptr;
+};
+
+class RefCount
+{
+    public:
+        RefCount() : counter(0) {};
+        ~RefCount();
+
+        unsigned int get() {
+            return counter;
+        }
+
+        // pre-increment (++counter)
+        void operator++(){
+            counter++;
+        }
+        // pre increment (counter++)
+        void operator++(int) {
+            counter++;
+        }
+
+        void operator--(){
+            counter--;
+        }
+        void operator--(int){
+            counter--;
+        }
+    private:
+        unsigned int counter;
+};
+
+template<typename T> class shared_ptr
+{
+    public:
+        shared_ptr(T* ptr) : raw_ptr(ptr) {
+            refCount = new RefCount();
+
+            if( raw_ptr != nullptr) {
+                (*refCount)++;
+            }
+        };
+
+        shared_ptr(const T* ptr) {
+            raw_ptr = ptr;
+            refCount = ptr->refCount;
+            (*refCount)++;
+        }
+
+        ~shared_ptr() {
+            (*refCount)--;
+
+            if ( refCount->get() == 0 ) {
+                delete raw_ptr;
+                delete refCount;
+            }
+        }
+
+        T& operator*() {
+            return *raw_ptr;
+        }
+
+        T* operator->() {
+            return raw_ptr;
+        }
+
+    private:
+        RefCount* refCount;
         T* raw_ptr;
 };
 
